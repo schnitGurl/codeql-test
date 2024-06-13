b4d49346a56ff518a65b8a408160c3a70f32e969
schnitgurL <146375954+schnitGurl@users.noreply.github.com>
2024-06-12 22:38:46 -0400

    Update ex1SL2.c
    Using filesize calcs

--- ex1SL2.c
+++ ex1SL2.c
@@ -1,17 +1,37 @@
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
-    
-#define BUFSIZE 256
-    
+#include <stdlib.h>
+
 // This program prints the size of a specified file in bytes
-int main(int argc, char** argv) {
+int main(int argc, char** argv) 
+{
     // Ensure that the user supplied exactly one command line argument
-    if (argc != 2) { 
-    fprintf(stderr, "Please provide the address of a file as an input.\n");
-    return -1;
+    if (argc != 2) 
+    {
+        fprintf(stderr, "Please provide the address of a file as an input.\n");
+        return -1;
     }
-    char cmd[BUFSIZE] = "wc -c < ";
-    strcat(cmd, argv[1]);
-    system(cmd);
+
+    // Open the file in read mode
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return -1;
+    }
+
+    // Seek to the end of the file to determine its size
+    fseek(file, 0, SEEK_END);
+    long size = ftell(file);
+    // Close the file
+    fclose(file);
+
+    // Check if the size was determined successfully
+    if (size == -1) {
+        perror("Error determining file size");
+        return -1;
+    }
+
+    // Print the size of the file
+    printf("The size of the file is: %ld bytes\n", size);
+
+    return 0;
 }
